7a0f2093c482c6fbf2135755beaa9d3ad1b16f8c
sebastiankurant <sebastian.kt7@gmail.com>
2017-06-12 15:45:11 +0200

    Create test which check constructor behavior with wrong arguments

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,10 +1,46 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ObjectArrayArguments;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
 
 public class CellTest {
 
+//    @ParameterizedTest
+//    @MethodSource(names = "createWordsWithLength")
+//    void withMethodSource(String word, int length) { }
+//
+//    private static Stream<Arguments> createWordsWithLength() {
+//        return Stream.of(
+//                ObjectArrayArguments.create("Hello", 5),
+//                ObjectArrayArguments.create("JUnit 5", 7));
+//    }
 
+    private static Stream<Arguments> createWrongConstructorParameters(){
+        return Stream.of(
+                ObjectArrayArguments.create(0,2),
+                ObjectArrayArguments.create(3,-1),
+                ObjectArrayArguments.create(-1,-1)
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource(names="createWrongConstructorParameters")
+    @DisplayName("Constructor throw Exepction when given parameters are below one and above three")
+    void testConstructorThrowsIllegalArgumentExpectionIfGivenParametersIsWrong(Integer row, Integer column){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Cell(row, column);
+        });
+    }
+
+//    @Test
+//    void testClearSetContentParameterToEmptyString(){
+//
+//    }
 
 }
