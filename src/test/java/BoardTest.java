b45a9976788c4d4d1d8b00f7212de89130793ef5
sebastiankurant <sebastian.kt7@gmail.com>
2017-06-12 18:23:52 +0200

    Fix arguments given in constructor test in BoardTest

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,41 +1,29 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import game.Board;
 import game.Cell;
-import game.Seed;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ObjectArrayArguments;
-import org.mockito.Mockito;
-
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.stream.Stream;
 
 public class BoardTest {
 
     @Test
     void testInitMethodCreateCorrectCells(){
-        Borad borad = new Board();
-        borad.init();
-        Cell[][] cells = createPrimitiveListWithMockedCells();
-        Cell cell = mock(Cell.class);
-        when(new Cell(anyInt(), anyInt())).thenReturn(cell);
-        assertEquals(cells ,borad.getBoard);
+        Board board = new Board();
+        board.init();
+        assertTrue(isCellObjectsInArrayAreEquals(board.getCells()));
     }
 
-    private Cell[][] createPrimitiveListWithMockedCells(){
-        Cell[][] cells = new Cell[3][3];
-        for (Cell[] rowCells : cells) {
-            for (Cell cell : rowCells) {
-                cell = mock(Cell.class);
+    private boolean isCellObjectsInArrayAreEquals(Cell[][] cells){
+        Integer rowRange = 3;
+        Integer columnRange = 3;
+        for (int row = 0; row < rowRange; row++) {
+            for (int column = 0; column < columnRange; column++) {
+                if(!cells[row][column].equals(new Cell(row, column))){
+                    return false;
+                }
             }
         }
-        return cells;
+        return true;
     }
 }
