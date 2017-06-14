7dc4b3c6f9589de90b409543aac04b79544a82d7
castarox <mika.kamil12@gmail.com>
2017-06-14 11:35:00 +0200

    Create Lupus

---
+++ src/main/java/game/ViewConsole.java
@@ -0,0 +1,74 @@
+package game;
+
+import java.io.PrintStream;
+import java.util.List;
+
+public class ViewConsole {
+
+    public ViewConsole(PrintStream outputStream){
+        System.setOut(outputStream);
+    }
+
+    public void printBoard(List<List<Cell>> board){
+        BoardIterator boardIterator = new BoardIterator(board);
+        Integer seedPrinted = 0;
+        StringBuilder boardToPrint = new StringBuilder();
+        while (boardIterator.hasNext()){
+            Character seedToAdd = chooseCrossNoughtOrEmpty(boardIterator.next());
+            boardToPrint.append(seedToAdd);
+            seedPrinted++;
+            String wallFloorOrEnd = chooseWallFloorOrEnd(seedPrinted);
+            boardToPrint.append(wallFloorOrEnd);
+        }
+        System.out.println(boardToPrint.toString());
+    }
+
+    private Character chooseCrossNoughtOrEmpty(Cell cell){
+        if (cell.getSeed() == Seed.CROSS){
+            return 'X';
+        }
+        else if (cell.getSeed() == Seed.NOUGHT) {
+            return 'O';
+        }
+        return ' ';
+    }
+
+    private String chooseWallFloorOrEnd(Integer seedPrinted){
+        if (seedPrinted == 9 ){
+            return "";
+        }
+        else if (seedPrinted % 3 == 0){
+            return "\n-----\n";
+        }
+        return "|";
+    }
+
+    public void getNameMessage(Integer playerIndex){
+        Integer playerNumber = playerIndex + 1;
+        System.out.println("Player " + playerNumber.toString() + " enter Name");
+    }
+
+    public void wrongInputMessage(){
+        System.out.println("Wrong input try again");
+    }
+
+    public void getRowMessage(){
+        System.out.println("Enter row number (1,3)");
+    }
+
+    public void getColumnMessage(){
+        System.out.println("Enter column number (1,3)");
+    }
+
+    public void winMessage(String name){
+        System.out.println("Player " + name + " WIN!!!");
+    }
+
+    public void drawMessage(){
+        System.out.println("DRAW");
+    }
+
+    public void turnMessage(String name){
+        System.out.println("Player " + name + " turn");
+    }
+}
