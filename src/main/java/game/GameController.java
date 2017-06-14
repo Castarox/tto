25798f72643bbba2fd3ded78b800562e85f9eef8
sebastiankurant <sebastian.kt7@gmail.com>
2017-06-14 11:35:44 +0200

    Create constellation Vigro

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -1,21 +1,19 @@
 package game;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class GameController {
     private Game game;
     private UserInput userInput;
     private List<Player> playerList;
 
-    public GameController(List<Player> playerList) {
+    public GameController(List<Player> playerList, GameState gameState, Board board, Integer moveCounter, UserInput userInput) {
         this.playerList = playerList;
-        this.game = new Game(this.randomPlayer(), GameState.PLAYING, new Board(), 0, this.playerList);
-        this.userInput = new UserInput(System.in);
+        this.game = new Game(this.randomPlayer(this.playerList), gameState, board, moveCounter, this.playerList);
+        this.userInput = userInput;
     }
 
-    public static void initGame() {
+    public static List<Player> getPlayers(){
         List<Player> playerList = new ArrayList<>();
         UserInput userInput = new UserInput(System.in);
         String firstPlayerName = userInput.getName();
@@ -24,32 +22,71 @@ public class GameController {
         Player secondPlayer = new Player(secondPlayerName, Seed.NOUGHT);
         playerList.add(firstPlayer);
         playerList.add(secondPlayer);
-        GameController gameController = new GameController(playerList);
+        return playerList;
+    }
+
+    public static void initGame() {
+        List<Player> playerList = GameController.getPlayers();
+        GameController gameController = new GameController(playerList, GameState.PLAYING, new Board(), 0, new UserInput(System.in));
         gameController.runGame();
+    }
+
+    public Map<String, Integer> getUserMove() {
+        Map<String, Integer> playerMove = new HashMap<>();
+        Integer row = this.userInput.getInteger();
+        Integer column = this.userInput.getInteger();
+        playerMove.put("row", row);
+        playerMove.put("column", column);
+        return playerMove;
+    }
+
+    public void updateGame(){
+        Map<String, Integer> playerMove = this.getUserMove();
+        this.game.getCurrentPlayer().makeMove(playerMove.get("row"), playerMove.get("column"));
+        this.game.updateBoard(this.game.getCurrentPlayer());
+        this.game.incrementMoves();
     }
 
     public void runGame() {
         while (true) {
-            Integer row = this.userInput.getInteger();
-            Integer column = this.userInput.getInteger();
-            this.game.getCurrentPlayer().makeMove(row, column);
-            this.game.updateBoard(this.game.getCurrentPlayer());
-            this.game.incrementMoves();
-            if (this.game.isWin()) {
-                System.out.println("You won" + this.game.getCurrentPlayer().getName());
+            updateGame();
+
+            if (Win()) {
                 break;
             }
-            if (this.game.isDraw()) {
-                System.out.println("Draw");
+
+            if (Draw()) {
                 break;
             }
-            this.game.switchPlayer();
+
+            nextPlayer();
         }
     }
 
-    public Player randomPlayer() {
+    public boolean Win() {
+        if (this.game.isWin()) {
+            System.out.println("You won" + this.game.getCurrentPlayer().getName());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean Draw() {
+        if (this.game.isDraw()) {
+            System.out.println("Draw");
+            return true;
+        }
+        return false;
+    }
+
+    public void nextPlayer() {
+        this.game.switchPlayer();
+    }
+
+
+    public Player randomPlayer(List<Player> playerList) {
         Random randomizer = new Random();
-        Player randomPlayer = this.playerList.get(randomizer.nextInt(this.playerList.size()));
+        Player randomPlayer = playerList.get(randomizer.nextInt(playerList.size()));
         return randomPlayer;
     }
 }
