e7ba5a464231031d674cd8355898c044987236e2
sebastiankurant <sebastian.kt7@gmail.com>
2017-06-16 08:33:07 +0200

    Create constellation Cetuss

--- src/test/java/game/controller/GameControllerTest.java
+++ src/test/java/game/controller/GameControllerTest.java
@@ -53,15 +53,11 @@ class GameControllerTest {
         List<Player> expected = new ArrayList<>();
         expected.add(new Player("sample", Seed.CROSS));
         expected.add(new Player("sample", Seed.NOUGHT));
-
         UserInput userInput = mock(UserInput.class);
         ViewConsole viewConsole = mock(ViewConsole.class);
-
         this.gameController.setViewConsole(viewConsole);
         this.gameController.setUserInput(userInput);
-
         when(userInput.getName()).thenReturn("sample");
-
         assertEquals(expected, this.gameController.getPlayers());
     }
 
@@ -83,16 +79,12 @@ class GameControllerTest {
     @Test
     void testGetPreparedGameReturnNotNull() {
         List<Player> playerList = new ArrayList<>();
-
         Player firstPlayer = mock(Player.class);
         Player secondPlayer = mock(Player.class);
-
         playerList.add(firstPlayer);
         playerList.add(secondPlayer);
-
         this.gameController.setPlayerList(playerList);
         when(this.gameController.randomPlayer(playerList)).thenReturn(null);
-
         assertNotNull(this.gameController.getPreparedGame());
     }
 
@@ -102,18 +94,13 @@ class GameControllerTest {
         Map<String, Integer> expected = new HashMap<>();
         expected.put("column", 0);
         expected.put("row", 0);
-
         ViewConsole viewConsole = mock(ViewConsole.class);
         UserInput userInput = mock(UserInput.class);
-
         when(userInput.getInteger()).thenReturn(1);
-
         this.gameController.setViewConsole(viewConsole);
         this.gameController.setUserInput(userInput);
-
         when(this.gameController.getColumnFromPlayer()).thenReturn(1);
         when(this.gameController.getRowFromPlayer()).thenReturn(1);
-
         assertEquals(expected, this.gameController.getUserMove());
     }
 
